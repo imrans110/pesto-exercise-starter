9180a1cd15ceee952b9697e864947242f8a7bd0a
Syed Imran Abidi <imrans110@hotmail.com>
2020-03-07 02:25:46 +0530

    Finish sumFibs & Complete Day-1 Exercises

--- js-exercises/sum-fibs/sumFibs.js
+++ js-exercises/sum-fibs/sumFibs.js
@@ -1,7 +1,30 @@
-function sumFibs() {
+function fibonacci(num) {
+  let secondNum = 1,
+    firstNum = 0,
+    temp;
 
+  while (num >= 0) {
+    temp = secondNum;
+    secondNum += firstNum;
+    firstNum = temp;
+    num--;
+  }
+
+  return firstNum;
 }
 
-export {
-  sumFibs,
-};
+function sumFibs(num) {
+  const fibonacciArr = [1];
+  let fibonacciNumber = 1;
+  while (fibonacciArr[fibonacciArr.length - 1] < num) {
+    fibonacciArr.push(fibonacci(fibonacciNumber));
+    fibonacciNumber++;
+  }
+  const oddValues = fibonacciArr.filter(
+    value => value % 2 !== 0 && value <= num
+  );
+  const oddValuesSum = oddValues.reduce((sum, value) => sum + value);
+  return oddValuesSum;
+}
+
+export { sumFibs };
