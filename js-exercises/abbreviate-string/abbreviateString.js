883331b6cad2247646dadfff6fbfff5786588946
Syed Imran Abidi <imrans110@hotmail.com>
2020-03-07 20:34:20 +0530

    Complete abbrevatedString

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,5 +1,18 @@
+// const isString = require("util").isString;
 import { isString } from "util";
 
-function abbreviateString(str) {}
+function abbreviateString(str) {
+  if (!isString(str)) {
+    throw new Error(`Expected a string in parameter.`);
+  }
+
+  const wordsInArray = str.split(" ");
+  const abbrevatedWord = wordsInArray[0];
+  const abbrevatedLetter = wordsInArray[wordsInArray.length - 1]
+    .split("")[0]
+    .toUpperCase();
+
+  return `${abbrevatedWord} ${abbrevatedLetter}.`;
+}
 
 export { abbreviateString };
