5ac73c4c12939482851a9fe5daf6bb0bd7fa35e8
Syed Imran Abidi <imrans110@hotmail.com>
2020-03-07 23:38:51 +0530

    Complete rot-13

--- js-exercises/rot-13/rot13.js
+++ js-exercises/rot-13/rot13.js
@@ -1,7 +1,27 @@
-function rot13(...args) {
-  return args;
+function rot13(encodedString) {
+  const ROT_13_VALUE = 13;
+  const LOWER_BOUND_CHAR_ASCII = 65;
+  const UPPER_BOUND_CHAR_ASCII = 90;
+
+  let decodedString = "";
+
+  for (let char of encodedString) {
+    const charASCIIValue = char.charCodeAt(0);
+    if (charASCIIValue >= 65 && charASCIIValue < 91) {
+      if (charASCIIValue + ROT_13_VALUE > UPPER_BOUND_CHAR_ASCII) {
+        const ASCIIValueDifference =
+          UPPER_BOUND_CHAR_ASCII - charASCIIValue + 1;
+        decodedString += String.fromCharCode(
+          ROT_13_VALUE - ASCIIValueDifference + LOWER_BOUND_CHAR_ASCII
+        );
+      } else {
+        decodedString += String.fromCharCode(charASCIIValue + ROT_13_VALUE);
+      }
+    } else {
+      decodedString += String.fromCharCode(charASCIIValue);
+    }
+  }
+  return decodedString;
 }
 
-export {
-  rot13,
-};
+export { rot13 };
