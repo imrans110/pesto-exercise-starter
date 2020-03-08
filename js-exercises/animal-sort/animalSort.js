9bf6317284949a195f1e7333c907c0391188f44d
Syed Imran Abidi <imrans110@hotmail.com>
2020-03-08 14:31:03 +0530

    Complete animalSort

--- js-exercises/animal-sort/animalSort.js
+++ js-exercises/animal-sort/animalSort.js
@@ -1,3 +1,18 @@
-const animalSort = animals => {};
+const animalSort = animals => {
+  return animals.sort(compareAnimal);
+};
+
+function compareAnimal(animal, nextAnimal) {
+  if (animal.numberOfLegs === nextAnimal.numberOfLegs) {
+    if (animal.name > nextAnimal.name) {
+      return 1;
+    }
+    return -1;
+  }
+  if (animal.numberOfLegs > nextAnimal.numberOfLegs) {
+    return 1;
+  }
+  return -1;
+}
 
 export { animalSort };
