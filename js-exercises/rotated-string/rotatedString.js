4a445c76ffa5f0990e65e7006825c1ac7c1003d8
Syed Imran Abidi <imrans110@hotmail.com>
2020-03-08 13:31:10 +0530

    Complete rotateString

--- js-exercises/rotated-string/rotatedString.js
+++ js-exercises/rotated-string/rotatedString.js
@@ -1,3 +1,19 @@
-const rotatedString = (str1, str2) => {};
+const shiftStringByOnePlace = str => {
+  return str.slice(1) + str.charAt(0);
+};
+
+const rotatedString = (str1, str2) => {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  for (let i = 0; i < str1.length - 1; i++) {
+    let shiftedString = shiftStringByOnePlace(str2);
+    if (str1 === shiftedString) {
+      return true;
+    }
+    str2 = shiftedString;
+  }
+  return false;
+};
 
 export { rotatedString };
