88dcae1f0972e4b125086091c4707cc4a30139cb
Syed Imran Abidi <imrans110@hotmail.com>
2020-03-09 19:06:06 +0530

    Complete Day-4 exercises Except Float Bug In SumOfInterest

--- js-exercises/account-dataset/accountDataset.js
+++ js-exercises/account-dataset/accountDataset.js
@@ -8,15 +8,58 @@ const fileContent = fs.readFileSync(
 
 const { bankBalances } = JSON.parse(fileContent);
 
-function hundredThousandairs() {}
+function hundredThousandairs() {
+  const MINIMUM_AMOUNT = 100000;
+  return bankBalances.filter(account => account.amount > MINIMUM_AMOUNT);
+}
 
-function datasetWithRoundedDollar() {}
+function datasetWithRoundedDollar() {
+  return bankBalances.map(account => {
+    return { ...account, rounded: Math.round(account.amount) };
+  });
+}
 
-function sumOfBankBalances() {}
+function sumOfBankBalances() {
+  let unRoundedSum = bankBalances.reduce(
+    (accumulator, nextAccount) => accumulator + parseFloat(nextAccount.amount),
+    0
+  );
+  return parseFloat(unRoundedSum.toFixed(2));
+}
 
-function sumOfInterests() {}
+function sumOfInterests() {
+  const SELECTED_STATES = ["WI", "IL", "WY", "OH", "GA", "DE"];
+  const INTEREST_MULTIPLIER = 0.189;
+  let unRoundedInterest = bankBalances.reduce((accumulator, nextAccount) => {
+    if (SELECTED_STATES.includes(nextAccount.state)) {
+      return accumulator + nextAccount.amount * INTEREST_MULTIPLIER;
+    }
+    return accumulator;
+  }, 0.0);
+  return unRoundedInterest;
+}
 
-function higherStateSums() {}
+function higherStateSums() {
+  const stateAggregatAmounts = {};
+  for (let account of bankBalances) {
+    if (stateAggregatAmounts[account.state]) {
+      stateAggregatAmounts[account.state] += parseFloat(account.amount);
+    } else {
+      stateAggregatAmounts[account.state] = parseFloat(account.amount);
+    }
+  }
+  const sumAmount = Object.keys(stateAggregatAmounts).reduce(
+    (accumulatedAmount, state) => {
+      if (stateAggregatAmounts[state] > 1000000) {
+        accumulatedAmount += stateAggregatAmounts[state];
+        return accumulatedAmount;
+      }
+      return accumulatedAmount;
+    },
+    0
+  );
+  return sumAmount;
+}
 
 export {
   hundredThousandairs,
