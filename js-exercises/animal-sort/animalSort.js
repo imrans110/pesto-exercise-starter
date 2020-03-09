43f65605a9128c2af473c7843e61a43f10ae1f60
Syed Imran Abidi <imrans110@hotmail.com>
2020-03-09 13:05:09 +0530

    Remove IF-ELSE block and Add Ternary

--- js-exercises/animal-sort/animalSort.js
+++ js-exercises/animal-sort/animalSort.js
@@ -6,15 +6,9 @@ function compareAnimal(animal, nextAnimal) {
   if (animal.numberOfLegs === nextAnimal.numberOfLegs) {
     const animalName = animal.name.toLowerCase();
     const nextAnimalName = nextAnimal.name.toLowerCase();
-    if (animalName > nextAnimalName) {
-      return 1;
-    }
-    return -1;
+    return animalName > nextAnimalName ? 1 : -1;
   }
-  if (animal.numberOfLegs > nextAnimal.numberOfLegs) {
-    return 1;
-  }
-  return -1;
+  return animal.numberOfLegs > nextAnimal.numberOfLegs ? 1 : -1;
 }
 
 export { animalSort };
