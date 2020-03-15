cffce5ee88f5855deaecc30bc54bbe3577f6e618
Syed Imran Abidi <imrans110@hotmail.com>
2020-03-15 22:23:13 +0530

    Complete Alternating Characters

--- js-exercises/alternating-characters/alternatingCharacters.js
+++ js-exercises/alternating-characters/alternatingCharacters.js
@@ -1,5 +1,14 @@
+/* eslint-disable no-plusplus */
 function alternatingCharacters(arr) {
-  return arr;
+  const deleteCounts = new Array(arr.length).fill(0);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === arr[i][j + 1]) {
+        deleteCounts[i]++;
+      }
+    }
+  }
+  return deleteCounts;
 }
 
 export { alternatingCharacters };
